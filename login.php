d29e32ebcc74a7eeb4a2986df1e6062ec459170b
joseph <1615021@rgu.ac.uk>
2017-04-19 22:16:21 +0100

    php

--- login.php
+++ login.php
@@ -9,7 +9,7 @@ include("dbConnect.php");
 
 
 
-if (empty($_POST["username"]) || empty($_POST["password"])) {
+if (empty($_POST['username']) || empty($_POST['password'])) {
     echo "Both fields are required.";
 } else {
 
@@ -25,11 +25,11 @@ if (empty($_POST["username"]) || empty($_POST["password"])) {
     //$sql = "SELECT uid FROM users WHERE username='$username' and password='$password'";
      $result = mysqli_query($link, $sql_query);
 
-    while($row = $result->fetch_array()){
-        // print out fields from row of data
-        echo "<p>".$row ['uid']. "</p>";
-
-    }
+//    while($row = $result->fetch_array()){
+//        // print out fields from row of data
+//        echo "<p>".$row ['uid']. "</p>";
+//
+//    }
 
     if (mysqli_num_rows($result) == 1) {
         header("location: profile.php"); // Redirecting To another Page
