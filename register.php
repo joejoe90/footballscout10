3e38b6c82cf88313902df90299c48c9c55d63843
joseph <1615021@rgu.ac.uk>
2017-04-19 16:33:05 +0100

    php

--- register.php
+++ register.php
@@ -22,15 +22,27 @@ if (isset($_POST['username']) && isset($_POST['password'])){
 <form class="form-signin" method="POST">
     <h2 class="form-signin-heading">Please Register</h2>
     <div class="input-group">
-        <span class="input-group-addon" id="basic-addon1">@</span>
-        <input type="text" name="username" class="form-control" placeholder="Username" required>
+        <Label class="input-group-addon" id="basic-addon1">First Name</Label>
+        <input type="text" name="firstname" class="form-control" placeholder="Enter Firstname" required>
+
+        <Label class="input-group-addon" id="basic-addon1">Last Name</Label>
+        <input type="text" name="lastname" class="form-control" placeholder="Enter lastname" required>
+
+        <Label class="input-group-addon" id="basic-addon1">age</Label>
+        <input type="number" name="age" class="form-control" placeholder="Enter age" required>
+
+        <Label class="input-group-addon" id="basic-addon1">position</Label>
+        <input type="text" name="position" class="form-control" placeholder="Enter position" required>
+
+        <label for="inputEmail" class="sr-only">Email address</label>
+        <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
+
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
+        <a class="btn btn-lg btn-primary btn-block" href="login.php">Login</a>
     </div>
-    <label for="inputEmail" class="sr-only">Email address</label>
-    <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-    <label for="inputPassword" class="sr-only">Password</label>
-    <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Password" required>
-    <button class="btn btn-lg btn-primary btn-block" type="submit">Register</button>
-    <a class="btn btn-lg btn-primary btn-block" href="login.php">Login</a>
 </form>
 </div>
 <html/>
