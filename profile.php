15f84effbb7f2c6cbdc4b03eac27ab22c1224cde
joseph <1615021@rgu.ac.uk>
2017-04-19 22:28:07 +0100

    php

--- profile.php
+++ profile.php
@@ -32,7 +32,7 @@
         <label><div align="right"><a href="index.php">logout</a></div></label>
     </div>
     <div>
-        <label width="129" rowspan="5"><img src="<?php echo $picture ?>" width="129" height="129" alt="no image found"/></label>
+        <label width="129" rowspan="5"><img src="profilepic.bmp" width="129" height="129" alt="no image found"/></label>
         <label width="82" valign="top"><div align="left">FirstName:</div></label>
         <label width="165" valign="top"><?php echo $firstname ?></label>
     </div>
