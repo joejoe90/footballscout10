a4c58dc53a17f578f33209435745a529a4cd10ab
joseph <1615021@rgu.ac.uk>
2017-04-21 15:08:52 +0100

    php

--- profile.php
+++ profile.php
@@ -85,8 +85,8 @@ if ($uploadOk == 0) {
 
     <div>
         <label width="129" rowspan="5"><img src="profilepic.bmp" width="129" height="129" alt="no image found"/></label>
-        <label width="82" valign="top"><div align="left">FirstName:</div></label>
-        <label width="165" valign="top"><?php echo $firstname ?></label>
+        <label width="82" valign="top"><div align="left">FirstName:  </div></label>
+        <label width="165" valign="top"><?php echo "$firstname" ?></label>
     </div>
 
     <div>
@@ -117,19 +117,19 @@ if ($uploadOk == 0) {
 
     <div>
         <label valign="top"><div align="left">LastName:</div></label>
-        <label valign="top"><?php echo $lastname ?></label>
+        <label valign="top"><?php echo "$lastname" ?></label>
     </div>
     <div>
         <label valign="top"><div align="left">username:</div></label>
-        <label valign="top"><?php echo $username ?></label>
+        <label valign="top"><?php echo "$username" ?></label>
     </div>
     <div>
         <label valign="top"><div align="left">Age:</div></label>
-        <label valign="top"><?php echo $age ?></label>
+        <label valign="top"><?php echo "$age" ?></label>
     </div>
     <div>
         <label valign="top"><div align="left">playerposition: </div></label>
-        <label valign="top"><?php echo $playerposition?></label>
+        <label valign="top"><?php echo "$playerposition"?></label>
     </div>
 </div>
 <p align="center"><a href="index.php"></a></p>
