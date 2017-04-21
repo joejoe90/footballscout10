a69c0e15606c18163ae3e8aabbb344a9c2740ac7
joseph <1615021@rgu.ac.uk>
2017-04-21 05:28:48 +0100

    php

--- profile.php
+++ profile.php
@@ -83,18 +83,19 @@ if ($uploadOk == 0) {
     </div>
 
     <div>
-    <form action="upload.php" method="post" enctype="multipart/form-data">
-        Select image to upload:
-        <input type="file" name="fileToUpload" id="fileToUpload">
-        <input type="submit" value="Upload Image" name="submit">
-    </form>
-    </div>
-
-    <div>
         <label width="129" rowspan="5"><img src="profilepic.bmp" width="129" height="129" alt="no image found"/></label>
         <label width="82" valign="top"><div align="left">FirstName:</div></label>
         <label width="165" valign="top"><?php echo $firstname ?></label>
     </div>
+
+    <div>
+        <form action="upload.php" method="post" enctype="multipart/form-data">
+            Select image to upload:
+            <input type="file" name="fileToUpload" id="fileToUpload">
+            <input type="submit" value="Upload Image" name="submit">
+        </form>
+    </div>
+
     <div>
         <label valign="top"><div align="left">LastName:</div></label>
         <label valign="top"><?php echo $lastname ?></label>
